c3c521f7cc88a77c2394f11c2cc63eddeafffe7a
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-15 09:44:25 +0100

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,47 @@
+export const nativeAds = [
+    {
+        id: "Temu",
+        image: "ads/temu.png",
+        title: "Sponsored",
+        subtitle: "legitimate online marketplace",
+        action: () => {
+            window.open("https://temu.to/k/eob8orcu3n0", "_blank")
+        }
+    },
+    {
+        id: "Stake.com",
+        image: "ads/stake.png",
+        title: "Sponsored",
+        subtitle: "The Global Giant of Crypto Gambling",
+        action: () => {
+            window.open("https://stake.com/?c=hcYKazFc", "_blank")
+        }
+    },
+    {
+        id: "bitoshi africa",
+        image: "ads/bitoshi.png",
+        title: "Sponsored",
+        subtitle: "swap, buy, sell, send and receive crypto with bitoshi africa",
+        action: () => {
+            window.open("https://bitoshi.africa/ref?username=ojareri", "_blank")
+        }
+    },
+    {
+        id: "palmpay",
+        image: "ads/palmpay.png",
+        title: "Sponsored",
+        subtitle: "enjoy free & safe transactions and cashbacks",
+        action: () => {
+            window.open("https://info.palmpay.com/tNYOx0uv", "_blank")
+        }
+    },
+    {
+        id: "opay",
+        image: "ads/opay.png",
+        title: "Sponsored",
+        subtitle: "enjoy free bank transfers, cashbacks and more",
+        action: () => {
+            window.open("https://opay.ng/s/2Tqtv", "_blank")
+        }
+    },
+];
